27466adb2bfc4d9fc34297729d05247f9afdb121
yingqiuli <yingqiuli@rrc.ca>
2024-03-13 22:42:22 -0500

    Search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,30 @@
 #     @books = Book.page(params[:page]).per(10)
 #   end
 # end
+# class BooksController < ApplicationController
+#   def index
+#     if params[:query].present?
+#       @books = Book.where("title LIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
+#     else
+#       @books = Book.page(params[:page]).per(10)
+#     end
+#   end
+
+#   def show
+#     @book = Book.find(params[:id])
+#   end
+
+#   def search
+#     @query = params[:query]
+#     @books = Book.where("title LIKE ?", "%#{@query}%")
+#   end
+# end
+
 class BooksController < ApplicationController
   def index
-    if params[:query].present?
-      @books = Book.where("title LIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
-    else
-      @books = Book.page(params[:page]).per(10)
-    end
+    @books = Book.all
+    @books = @books.joins(:genres).where(genres: { id: params[:genre_id] }) if params[:genre_id].present?
+    @books = @books.page(params[:page]).per(10)
   end
 
   def show
@@ -27,4 +44,3 @@ class BooksController < ApplicationController
     @books = Book.where("title LIKE ?", "%#{@query}%")
   end
 end
-
