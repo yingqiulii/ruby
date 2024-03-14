c07275c05ca384e889c2a1b887e0d224239a9b89
yingqiuli <yingqiuli@rrc.ca>
2024-03-14 00:16:34 -0500

    genre page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,4 @@
-# class GenresController < ApplicationController
-#   def show
-#     @genre = Genre.find(params[:id])
-#     @books = @genre.books
-#   end
-# end
+
 class GenresController < ApplicationController
   def index
     @genres = Genre.all
