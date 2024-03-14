d12c9336b74b7009b12ed31397f3e0dabad528fc
yingqiuli <yingqiuli@rrc.ca>
2024-03-13 22:00:22 -0500

    Simple Search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,30 @@
+# class BooksController < ApplicationController
+#   def index
+#     @books = Book.all
+#   end
+#   def show
+#     @book = Book.find(params[:id])
+#   end
+#   def index
+#     @books = Book.page(params[:page]).per(10)
+#   end
+# end
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    if params[:query].present?
+      @books = Book.where("title LIKE ?", "%#{params[:query]}%").page(params[:page]).per(10)
+    else
+      @books = Book.page(params[:page]).per(10)
+    end
   end
+
   def show
     @book = Book.find(params[:id])
   end
-  def index
-    @books = Book.page(params[:page]).per(10)
+
+  def search
+    @query = params[:query]
+    @books = Book.where("title LIKE ?", "%#{@query}%")
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,44 @@
-Rails.application.routes.draw do
-  get 'books/index'
+# Rails.application.routes.draw do
+#   get 'books/index'
 
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   get '/about', to: 'pages#about', as: 'about'
+
+#   resources :books
+#   root 'books#index'
+
+#   resources :books, only: [:show]
+
+#   resources :authors
+
+#   resources :publishers
+
+#   resources :books do
+#     collection do
+#       get 'search'
+#     end
+#   end
+
+
+# end
+Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get '/about', to: 'pages#about', as: 'about'
 
-  resources :books
-  root 'books#index'
+  get 'books/search', to: 'books#search', as: 'books_search'
 
-  resources :books, only: [:show]
+  resources :books
 
   resources :authors
-
   resources :publishers
 
+  root 'books#index'
+  resources :books do
+    collection do
+      get 'search', to: 'books#search'
+    end
+  end
 
 end
