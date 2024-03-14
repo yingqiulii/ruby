8b6def640892063e9f3f310e32e8576498dd6de2
yingqiuli <yingqiuli@rrc.ca>
2024-03-14 00:14:31 -0500

    genre page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,17 @@
+# class GenresController < ApplicationController
+#   def show
+#     @genre = Genre.find(params[:id])
+#     @books = @genre.books
+#   end
+# end
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+  def index
+    @genres = Genre.all.page(params[:page]).per(10)
+  end
   def show
     @genre = Genre.find(params[:id])
-    @books = @genre.books
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,3 +7,20 @@ class Book < ApplicationRecord
   validates :published_at, presence: true
   validates :isbn, presence: true
 end
+# class Book < ApplicationRecord
+#   belongs_to :author
+#   belongs_to :publisher
+#   has_and_belongs_to_many :genres
+
+#   validates :title, presence: true
+#   validates :published_at, presence: true
+#   validates :isbn, presence: true
+
+#   before_validation :set_default_genre
+
+#   private
+
+#   def set_default_genre
+#     self.genre ||= nil
+#   end
+# end
